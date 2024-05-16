70e811fc6c7be297e02b3b0c9a86e82873da7c6c
Cristian Gordon <coralcristian5@gmail.com>
2024-05-16 16:13:26 -0500

    create others private routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,20 +6,31 @@ import "dotenv/config";
 const router = express.Router();
 
 router.post("/api/users", userControllers.create);
-router.get("/api/users/:id", userControllers.find);
-router.get("/api/users", userControllers.list);
-router.patch("/api/users/:id", userControllers.update);
-router.delete("/api/users/:id", userControllers.destroy);
+router.get(
+  "/api/users/:id",
+  expressjwt({ algorithms: ["HS256"], secret: process.env.JWT_SECRET }),
+  userControllers.find
+);
+router.get(
+  "/api/users",
+  expressjwt({ algorithms: ["HS256"], secret: process.env.JWT_SECRET }),
+  userControllers.list
+);
+router.patch(
+  "/api/users/:id",
+  expressjwt({ algorithms: ["HS256"], secret: process.env.JWT_SECRET }),
+  userControllers.update
+);
+router.delete(
+  "/api/users/:id",
+  expressjwt({ algorithms: ["HS256"], secret: process.env.JWT_SECRET }),
+  userControllers.destroy
+);
 router.post("/api/users/login", userControllers.login);
 router.post(
   "/api/users/profile",
   expressjwt({ algorithms: ["HS256"], secret: process.env.JWT_SECRET }),
   userControllers.profile
-);
-router.post(
-  "/api/users/comprar",
-  expressjwt({ algorithms: ["HS256"], secret: process.env.JWT_SECRET }),
-  userControllers.comprar
 );
 
 export default router;
