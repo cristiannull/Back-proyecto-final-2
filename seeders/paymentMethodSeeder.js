d5e37965d229e2c67d0c68d491b8b6c7b007ea8a
DevelopmentSystem <gtovar@dsystem.co>
2024-05-18 02:00:20 -0500

    create Seeders of pegi, theme and paymentMethod

---
+++ seeders/paymentMethodSeeder.js
@@ -0,0 +1,28 @@
+import PaymentMethod from "../models/PaymentMethod.js";
+
+await PaymentMethod.create({
+  _id: "66484f81d185e084c7b0b126",
+  name: "falabellacard",
+});
+
+await PaymentMethod.create({
+  _id: "66484f8dd185e084c7b0b128",
+  name: "bbvacard",
+});
+
+await PaymentMethod.create({
+  _id: "66484f98d185e084c7b0b12a",
+  name: "mastercard",
+});
+
+await PaymentMethod.create({
+    _id: "66484f9ed185e084c7b0b12c",
+    name: "visacard",
+  });
+  
+  await PaymentMethod.create({
+    _id: "66484fa8d185e084c7b0b12e",
+    name: "goldencard",
+  });
+
+console.log("paymentMethods creados exitocisimamente bien");
---
+++ seeders/pegiSeeder.js
@@ -0,0 +1,29 @@
+import Pegi from "../models/pegi.js";
+
+await Pegi.create({
+  _id: "664846fda7e41a4b4a090abb",
+  name: "PEGI 3",
+});
+
+await Pegi.create({
+  _id: "6648470ca7e41a4b4a090abd",
+  name: "PEGI 7",
+});
+
+await Pegi.create({
+  _id: "66484719a7e41a4b4a090abf",
+  name: "PEGI 12",
+});
+
+await Pegi.create({
+    _id: "66484727a7e41a4b4a090ac1",
+    name: "PEGI 16",
+  });
+  
+  await Pegi.create({
+    _id: "6648473ba7e41a4b4a090ac3",
+    name: "PEGI 18",
+  });
+
+console.log("pegis creados exitocisimamente bien");
+
