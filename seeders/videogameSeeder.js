721cd9bb64cf4e53bcbf9db9c79fbc5d33e15c5f
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-05-20 16:53:30 -0500

    Changes to the videogame Seeder

--- seeders/videogameSeeder.js
+++ seeders/videogameSeeder.js
@@ -3,11 +3,12 @@ import VideoGame from "../models/VideoGame.js";
 await VideoGame.create({
   _id: "664695bacf29fe32661290d9",
   name: "Rocket League",
-  /*     category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+   category: "6647bb759c8f74c7a883b5b4",
+    pegi: "66484727a7e41a4b4a090ac1",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c8f0d5b0a90daa03fe2d",
+   /* image:"",  */
   price: "0",
   description:
     "Un juego que combina el fútbol y los autos. Los jugadores controlan vehículos equipados con cohetes y compiten para marcar goles en una arena futurista. Es conocido por su jugabilidad rápida y adictiva.",
@@ -16,11 +17,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695e8cf29fe32661290db",
   name: "God of War",
-  /*   category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+    category: "6647baff9c8f74c7a883b5b0",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c929d5b0a90daa03fe31",
+    /*image:"",  */
   price: "59",
   description:
     "Un juego de acción y aventura que sigue la historia de Kratos, un guerrero espartano, en su búsqueda de venganza contra los dioses griegos. La serie es famosa por su combate intenso y narración épica.",
@@ -29,11 +31,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "6646960dcf29fe32661290dd",
   name: "Roblox",
-  /*  category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+    category: "6647bb759c8f74c7a883b5b4",
+    pegi: "66484719a7e41a4b4a090abf",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c916d5b0a90daa03fe2f",
+   /* image:"",  */
   price: "0",
   description:
     "Una plataforma en línea que permite a los usuarios crear y compartir sus propios juegos y experiencias. Ofrece una gran variedad de géneros y estilos, con una comunidad activa que constantemente añade nuevo contenido.",
@@ -42,11 +45,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "66469619cf29fe32661290df",
   name: "Minecraft",
-  /* category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+  category: "6647baff9c8f74c7a883b5b0",
+    pegi: "66484719a7e41a4b4a090abf",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c8f0d5b0a90daa03fe2d",
+    /*image:"",  */
   price: "20",
   description:
     "Un juego de construcción y aventura donde los jugadores pueden explorar un mundo generado aleatoriamente, recolectar recursos y construir estructuras. Tiene modos de juego creativos y de supervivencia, fomentando la creatividad y la exploración.",
@@ -55,11 +59,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "66469625cf29fe32661290e1",
   name: "Terraria",
-  /* category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+   category: "6647baff9c8f74c7a883b5b0",
+    pegi: "66484719a7e41a4b4a090abf",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c929d5b0a90daa03fe31",
+   /* image:"",  */
   price: "10",
   description:
     " Un juego de aventura y construcción en 2D. Los jugadores exploran, recolectan recursos, luchan contra monstruos y construyen estructuras en un mundo abierto. Es conocido por su profundidad y variedad de contenido.",
@@ -68,11 +73,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "66469632cf29fe32661290e3",
   name: "Portal 2",
-  /*  category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+  category: "6647baff9c8f74c7a883b5b0",
+    pegi: "66484719a7e41a4b4a090abf",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c916d5b0a90daa03fe2f",
+    /*image:"",  */
   price: "12",
   description:
     " Un juego de puzzles en primera persona desarrollado por Valve. Los jugadores utilizan una pistola de portales para crear portales interconectados y resolver una serie de desafiantes rompecabezas. La narrativa y los personajes humorísticos son muy apreciados.",
@@ -81,11 +87,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "66469671cf29fe32661290e5",
   name: "The last of us",
-  /*     category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+    category: "6647baff9c8f74c7a883b5b0",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c916d5b0a90daa03fe2f",
+   /* image:"",  */
   price: "50",
   description:
     "Un juego de acción y aventura que sigue a Joel y Ellie en un viaje a través de un mundo postapocalíptico devastado por una infección fúngica. Es aclamado por su historia emotiva, personajes complejos y ambiente tenso.",
@@ -94,11 +101,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "66469686cf29fe32661290e7",
   name: "The last of us part 2",
-  /*  category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+    category: "6647baff9c8f74c7a883b5b0",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c929d5b0a90daa03fe31",
+    /*image:"",  */
   price: "65",
   description:
     "La secuela del primer juego, continúa la historia de Ellie en un viaje de venganza y redención. Mantiene la alta calidad en narrativa, jugabilidad y desarrollo de personajes.",
@@ -107,11 +115,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664696cecf29fe32661290e9",
   name: "Cyberpunk 2077",
-  /*  category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+    category: "6647baff9c8f74c7a883b5b0",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c929d5b0a90daa03fe31",
+    /*image:"",  */
   price: "45",
   description:
     "Un juego de rol de acción ambientado en una metrópolis futurista llamada Night City. Los jugadores controlan a V, un mercenario en busca de un implante único que podría ser la clave para la inmortalidad. Es conocido por su inmersiva atmósfera y opciones de personalización.",
@@ -120,11 +129,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664696e5cf29fe32661290eb",
   name: "Halo Reach",
-  /*  category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+   category: "6647bb759c8f74c7a883b5b4",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c929d5b0a90daa03fe31",
+   /* image:"",  */
   price: "12",
   description:
     "Una precuela de la serie Halo que cuenta la historia del equipo Noble, un grupo de soldados espartanos, y su lucha desesperada para salvar el planeta Reach de la invasión del Covenant. Es valorado por su campaña emotiva y multijugador robusto.",
@@ -133,11 +143,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664696facf29fe32661290ed",
   name: "Gears of War",
-  /*   category: "",
-  pegi: "",
-  gender: "",
-  theme: "",
-  image:"",  */
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer:"6647c929d5b0a90daa03fe31",
+  /*image:"",  */
   price: "10",
   description:
     "Un juego de disparos en tercera persona que sigue a un grupo de soldados conocidos como el Escuadrón Delta en su lucha contra una horda alienígena llamada Locust. Es famoso por su intensa acción, coberturas tácticas y narrativa cinematográfica.",
@@ -146,11 +157,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "66469710cf29fe32661290ef",
   name: "Call of Duty II",
-  /* category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+   category: "6647bb759c8f74c7a883b5b4",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c929d5b0a90daa03fe31",
+    /*image:"",  */
   price: "15",
   description:
     " Un juego de disparos en primera persona ambientado en la Segunda Guerra Mundial. Ofrece varias campañas que siguen a soldados de diferentes naciones y es conocido por su autenticidad histórica y acción trepidante.",
@@ -159,11 +171,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "6646975fcf29fe32661290f1",
   name: "Elden Ring",
-  /*     category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+   category: "6647bb759c8f74c7a883b5b4",
+    pegi: "66484719a7e41a4b4a090abf",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c916d5b0a90daa03fe2f",
+    /*image:"",  */
   price: "50",
   description:
     "Un juego de rol de acción desarrollado por FromSoftware y George R.R. Martin. Promete un vasto mundo abierto lleno de peligros, combates desafiantes y una rica mitología, manteniendo la dificultad y diseño característicos de los juegos de FromSoftware.",
@@ -172,11 +185,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "6646976acf29fe32661290f3",
   name: "GTA 5",
-  /*  category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+    category: "6647bb759c8f74c7a883b5b4",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c8f0d5b0a90daa03fe2d",
+   /* image:"",  */
   price: "20",
   description:
     "Un juego de acción y aventura en mundo abierto que sigue las historias entrelazadas de tres criminales en la ciudad ficticia de Los Santos. Es conocido por su vasta y detallada representación de la vida urbana, así como su jugabilidad diversa y narrativa humorística.",
@@ -185,11 +199,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "6646977ecf29fe32661290f5",
   name: "Resident Evil 4",
-  /* category: "",
-    pegi: "",
-    gender: "",
-    theme: "",
-    image:"",  */
+   category: "6647baff9c8f74c7a883b5b0",
+    pegi: "6648473ba7e41a4b4a090ac3",
+    gender: "6647c2afa55c56098a0045b5",
+    theme: "66484bf01593a60f58e0c0a3",
+    developer:"6647c8f0d5b0a90daa03fe2d",
+    /*image:"",  */
   price: "35",
   description:
     "Un juego de horror y supervivencia que sigue a Leon S. Kennedy en una misión para rescatar a la hija del presidente de Estados Unidos de un culto siniestro en una remota aldea europea. Es famoso por reinventar la serie con su enfoque en la acción y su innovador sistema de cámara.",
