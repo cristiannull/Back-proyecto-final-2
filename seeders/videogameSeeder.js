dbf504a6f83d747c517929092a54885b7f4d1eb7
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-05-31 17:08:26 -0500

    fix

--- seeders/videogameSeeder.js
+++ seeders/videogameSeeder.js
@@ -225,6 +225,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290da",
   name: "The Legend of Zelda: Breath of the Wild",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://zelda.nintendo.com/breath-of-the-wild/assets/media/wallpapers/tablet-1.jpg",
   price: 60,
@@ -235,6 +240,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290db",
   name: "Minecraft Dungeons",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://i.pinimg.com/736x/aa/e9/c3/aae9c33a756621b8209bdf7ce51b076a.jpg",
   price: 18,
@@ -245,6 +255,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290dc",
   name: "Fortnite",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://assetsio.gnwcdn.com/co2ekt.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
   price: 0,
@@ -255,6 +270,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290dd",
   name: "Super Mario Odyssey",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://media.vandal.net/m/43395/super-mario-odyssey-2017102611035_25.jpg",
   price: 60,
@@ -265,6 +285,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290de",
   name: "Red Dead Redemption 2",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png",
   price: 60,
@@ -275,6 +300,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290df",
   name: "The Witcher 3: Wild Hunt",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://media.vandal.net/m/15596/the-witcher-3-wild-hunt-201551495951_1.jpg",
   price: 40,
@@ -285,6 +315,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e0",
   name: "Overwatch 2",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://assetsio.gnwcdn.com/co5tkm.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
   price: 40,
@@ -295,6 +330,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e1",
   name: "Call of Duty: Modern Warfare",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://pics.filmaffinity.com/Call_of_Duty_Modern_Warfare-724589128-large.jpg",
   price: 60,
@@ -305,6 +345,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e2",
   name: "Grand Theft Auto San Adreas",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://static.wikia.nocookie.net/esgta/images/c/c2/Grand_Theft_Auto_San_Andreas.JPG/revision/latest?cb=20161127211015",
   price: 30,
@@ -315,6 +360,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e3",
   name: "Animal Crossing: New Horizons",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image:
     "https://media.vandal.net/m/65557/animal-crossing-new-horizons-202011012375667_1.jpg",
   price: 60,
@@ -325,7 +375,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e4",
   name: "Doom Eternal",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.f4278cd65095df6435cb4e86d1fa86cd?rik=z%2fJW1Nc10dfTwA&pid=ImgRaw&r=0",
   price: 60,
   description:
     "Un juego de disparos en primera persona lleno de acción donde el jugador combate hordas de demonios.",
@@ -333,17 +389,29 @@ await VideoGame.create({
 
 await VideoGame.create({
   _id: "664695bacf29fe32661290e5",
-  name: "Cyberpunk 2077",
-  image: "",
-  price: 60,
+  name: "GTA Vice City",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/OIP.5Zy-pog_QbRjALPiBADZugHaLH?rs=1&pid=ImgDetMain",
+  price: 20,
   description:
-    "Un RPG de mundo abierto ambientado en un futuro distópico donde el jugador explora la ciudad de Night City.",
+    "Un juego de acción y aventura ambientado en una ciudad ficticia inspirada en Miami en los años 80.",
 });
 
 await VideoGame.create({
   _id: "664695bacf29fe32661290e6",
   name: "Halo Infinite",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.580b7703c72ae29faeaa2b3f20bcb00a?rik=PdKnH1uasznnFQ&pid=ImgRaw&r=0",
   price: 60,
   description:
     "Un juego de disparos en primera persona que sigue la historia del Master Chief en su lucha contra el Covenant.",
@@ -352,7 +420,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e7",
   name: "Assassin's Creed Valhalla",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://images-na.ssl-images-amazon.com/images/I/91lmTAVXgHL.jpg",
   price: 60,
   description:
     "Un juego de acción y aventura donde el jugador controla a un vikingo explorando la Inglaterra del siglo IX.",
@@ -361,7 +434,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e8",
   name: "Resident Evil Village",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.389bd5a435f6b407b6e9bb069f3cf5cf?rik=pyQcznBZg8gd6A&riu=http%3a%2f%2fgamepreorders.com%2fwp-content%2fuploads%2f2021%2f01%2fcover-art-2.jpg&ehk=tB8JjGWAO6%2bNVGMw1N1NYvCDGBzNQftzrIAPRDdguLg%3d&risl=&pid=ImgRaw&r=0",
   price: 60,
   description:
     "Un juego de terror y supervivencia que sigue la historia de Ethan Winters en un misterioso pueblo europeo.",
@@ -370,7 +449,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290e9",
   name: "Hades",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://assets-prd.ignimgs.com/2020/09/17/hades-button-fin-1600382124693.png",
   price: 25,
   description:
     "Un juego de acción roguelike donde el jugador controla a Zagreus en su intento de escapar del Inframundo.",
@@ -379,7 +464,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290ea",
   name: "Among Us",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://errekgamer.com/wp-content/uploads/2021/12/Among-Us-portada.jpg",
   price: 5,
   description:
     "Un juego multijugador de deducción social donde los jugadores deben identificar al impostor en la nave espacial.",
@@ -388,7 +479,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290eb",
   name: "Ghost of Tsushima",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://noespaisparafrikis.com/wp-content/uploads/2020/07/analisis-ghost-of-tsushima-portada.jpg",
   price: 60,
   description:
     "Un juego de acción y aventura ambientado en el Japón feudal donde el jugador controla a un samurái en su lucha contra los invasores mongoles.",
@@ -397,7 +494,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290ec",
   name: "God of War",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://images-na.ssl-images-amazon.com/images/I/81gXqxyFrxL.jpg",
   price: 60,
   description:
     "Un juego de acción y aventura que sigue la historia de Kratos y su hijo Atreus en la mitología nórdica.",
@@ -406,7 +508,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290ed",
   name: "Splatoon 2",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.386faff5491962e1ee1d4ce3d281f913?rik=ltHbxq3AxUq5NQ&pid=ImgRaw&r=0",
   price: 60,
   description:
     "Un juego de disparos en tercera persona donde los jugadores compiten para cubrir el mayor área posible con tinta.",
@@ -415,7 +523,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290ee",
   name: "Persona 5",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/OIP.pHGu6F8sd7tUyhh16X557wHaKj?w=115&h=180&c=7&r=0&o=5&pid=1.7",
   price: 60,
   description:
     "Un juego de rol japonés que sigue la vida de un grupo de estudiantes de secundaria con habilidades sobrenaturales.",
@@ -424,7 +538,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290ef",
   name: "Final Fantasy VII Remake",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://gamefaqs.gamespot.com/a/box/8/5/3/746853_front.jpg",
   price: 60,
   description:
     "Una versión modernizada del clásico juego de rol japonés con gráficos mejorados y un sistema de combate actualizado.",
@@ -433,7 +552,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f0",
   name: "Sekiro: Shadows Die Twice",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/OIP.8hTIlUM1aggT1EneCN4S8wHaJS?rs=1&pid=ImgDetMain",
   price: 60,
   description:
     "Un juego de acción y aventura que sigue a un shinobi en su misión para rescatar a su señor y vengarse de sus enemigos.",
@@ -442,7 +567,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f1",
   name: "Mario Kart 8 Deluxe",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/OIP.2kLB_tNuUVNink9aMNl_qgHaHY?w=163&h=180&c=7&r=0&o=5&pid=1.7",
   price: 60,
   description:
     "Un juego de carreras de karts con personajes de la serie Mario y varios modos de juego divertidos.",
@@ -451,7 +582,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f2",
   name: "Horizon Zero Dawn",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.d995230ecc0850cdaa55933fe562256e?rik=oG4zP6JMw6On4g&pid=ImgRaw&r=0",
   price: 50,
   description:
     "Un juego de acción y aventura en un mundo post-apocalíptico donde los jugadores cazan máquinas peligrosas.",
@@ -460,7 +597,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f3",
   name: "Bloodborne",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.d436b06aa4f89a4c230a003bb74f404b?rik=mqDhuZRNlrWqkA&pid=ImgRaw&r=0",
   price: 20,
   description:
     "Un juego de rol y acción en un mundo gótico lleno de monstruos y secretos oscuros.",
@@ -469,7 +612,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f4",
   name: "The Elder Scrolls V: Skyrim",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg",
   price: 40,
   description:
     "Un RPG de mundo abierto donde los jugadores pueden explorar y vivir aventuras en la tierra de Skyrim.",
@@ -478,7 +627,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f5",
   name: "Dark Souls III",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://www.laps4.com/wp-content/uploads/2019/02/Celeste-portada.jpg",
   price: 60,
   description:
     "Un desafiante juego de rol de acción con combates intensos y un mundo interconectado.",
@@ -487,6 +641,11 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f6",
   name: "Celeste",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
   image: "",
   price: 20,
   description:
@@ -496,7 +655,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f7",
   name: "Control",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://gamepreorders.com/wp-content/uploads/2019/03/cover-art-6.jpg",
   price: 30,
   description:
     "Un juego de acción y aventura donde el jugador investiga fenómenos sobrenaturales en una misteriosa agencia gubernamental.",
@@ -505,7 +669,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f8",
   name: "Nier: Automata",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://th.bing.com/th/id/R.7f9bbcca499b8c51fc7c5174d0ea2290?rik=5LwRTZAQLwEw0w&pid=ImgRaw&r=0",
   price: 40,
   description:
     "Un juego de rol y acción que explora temas filosóficos a través de la historia de androides en un mundo post-apocalíptico.",
@@ -514,7 +684,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290f9",
   name: "Monster Hunter: World",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://1.bp.blogspot.com/-uZ6c1w8RAzw/XkYOeXeJQUI/AAAAAAAADWA/jjwZBulmRTMuMTSFvS61qGbcKsIOZj5jwCLcBGAsYHQ/s1600/monster-hunter-world-cover.jpg",
   price: 30,
   description:
     "Un juego de rol de acción donde los jugadores cazan monstruos gigantes en un mundo abierto.",
@@ -523,7 +699,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290fa",
   name: "Star Wars Jedi: Fallen Order",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://i.blogs.es/0f6803/jfo_key_deluxe_rgb/1366_2000.jpeg",
   price: 60,
   description:
     "Un juego de acción y aventura donde el jugador controla a un Jedi en su lucha contra el Imperio.",
@@ -532,7 +713,13 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290fb",
   name: "Cuphead",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image:
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/ae06dc63-c2dc-4715-a84e-624ca36052b7/dc05jjr-77d2bedb-512e-4d0c-af9c-badf1f85b638.jpg",
   price: 20,
   description:
     "Un juego de plataformas y disparos con un estilo artístico inspirado en los dibujos animados de los años 30.",
@@ -541,7 +728,12 @@ await VideoGame.create({
 await VideoGame.create({
   _id: "664695bacf29fe32661290fc",
   name: "Apex Legends",
-  image: "",
+  category: "6647bb759c8f74c7a883b5b4",
+  pegi: "6648473ba7e41a4b4a090ac3",
+  gender: "6647c2afa55c56098a0045b5",
+  theme: "66484bf01593a60f58e0c0a3",
+  developer: "6647c8f0d5b0a90daa03fe2d",
+  image: "https://assets.vg247.com/current/2019/02/apex_legends_main_art_2.jpg",
   price: 0,
   description:
     "Un juego de batalla real donde los jugadores compiten en equipos para ser los últimos en pie.",
