87130fdda0fdbb851ca49483b4813e28dc01c39a
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-05-31 16:05:35 -0500

    changes in seeder of videogameseeder

--- seeders/videogameSeeder.js
+++ seeders/videogameSeeder.js
@@ -8,8 +8,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c8f0d5b0a90daa03fe2d",
-   /* image:"",  */
-  price: "0",
+    image:"",  
+  price: 0,
   description:
     "Un juego que combina el fútbol y los autos. Los jugadores controlan vehículos equipados con cohetes y compiten para marcar goles en una arena futurista. Es conocido por su jugabilidad rápida y adictiva.",
 });
@@ -22,8 +22,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c929d5b0a90daa03fe31",
-    /*image:"",  */
-  price: "59",
+    image:"",  
+  price: 59,
   description:
     "Un juego de acción y aventura que sigue la historia de Kratos, un guerrero espartano, en su búsqueda de venganza contra los dioses griegos. La serie es famosa por su combate intenso y narración épica.",
 });
@@ -37,7 +37,7 @@ await VideoGame.create({
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c916d5b0a90daa03fe2f",
    /* image:"",  */
-  price: "0",
+  price: 0,
   description:
     "Una plataforma en línea que permite a los usuarios crear y compartir sus propios juegos y experiencias. Ofrece una gran variedad de géneros y estilos, con una comunidad activa que constantemente añade nuevo contenido.",
 });
@@ -50,8 +50,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c8f0d5b0a90daa03fe2d",
-    /*image:"",  */
-  price: "20",
+    image:"",  
+  price: 20,
   description:
     "Un juego de construcción y aventura donde los jugadores pueden explorar un mundo generado aleatoriamente, recolectar recursos y construir estructuras. Tiene modos de juego creativos y de supervivencia, fomentando la creatividad y la exploración.",
 });
@@ -65,7 +65,7 @@ await VideoGame.create({
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c929d5b0a90daa03fe31",
    /* image:"",  */
-  price: "10",
+  price: 10,
   description:
     " Un juego de aventura y construcción en 2D. Los jugadores exploran, recolectan recursos, luchan contra monstruos y construyen estructuras en un mundo abierto. Es conocido por su profundidad y variedad de contenido.",
 });
@@ -78,8 +78,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c916d5b0a90daa03fe2f",
-    /*image:"",  */
-  price: "12",
+    image:"",  
+  price: 12,
   description:
     " Un juego de puzzles en primera persona desarrollado por Valve. Los jugadores utilizan una pistola de portales para crear portales interconectados y resolver una serie de desafiantes rompecabezas. La narrativa y los personajes humorísticos son muy apreciados.",
 });
@@ -92,8 +92,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c916d5b0a90daa03fe2f",
-   /* image:"",  */
-  price: "50",
+    image:"",  
+  price: 50,
   description:
     "Un juego de acción y aventura que sigue a Joel y Ellie en un viaje a través de un mundo postapocalíptico devastado por una infección fúngica. Es aclamado por su historia emotiva, personajes complejos y ambiente tenso.",
 });
@@ -106,8 +106,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c929d5b0a90daa03fe31",
-    /*image:"",  */
-  price: "65",
+    image:"",  
+  price: 65,
   description:
     "La secuela del primer juego, continúa la historia de Ellie en un viaje de venganza y redención. Mantiene la alta calidad en narrativa, jugabilidad y desarrollo de personajes.",
 });
@@ -120,8 +120,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c929d5b0a90daa03fe31",
-    /*image:"",  */
-  price: "45",
+    image:"",  
+  price: 45,
   description:
     "Un juego de rol de acción ambientado en una metrópolis futurista llamada Night City. Los jugadores controlan a V, un mercenario en busca de un implante único que podría ser la clave para la inmortalidad. Es conocido por su inmersiva atmósfera y opciones de personalización.",
 });
@@ -134,8 +134,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c929d5b0a90daa03fe31",
-   /* image:"",  */
-  price: "12",
+    image:"",  
+  price: 12,
   description:
     "Una precuela de la serie Halo que cuenta la historia del equipo Noble, un grupo de soldados espartanos, y su lucha desesperada para salvar el planeta Reach de la invasión del Covenant. Es valorado por su campaña emotiva y multijugador robusto.",
 });
@@ -148,8 +148,8 @@ await VideoGame.create({
   gender: "6647c2afa55c56098a0045b5",
   theme: "66484bf01593a60f58e0c0a3",
   developer:"6647c929d5b0a90daa03fe31",
-  /*image:"",  */
-  price: "10",
+  image:"",  
+  price: 10,
   description:
     "Un juego de disparos en tercera persona que sigue a un grupo de soldados conocidos como el Escuadrón Delta en su lucha contra una horda alienígena llamada Locust. Es famoso por su intensa acción, coberturas tácticas y narrativa cinematográfica.",
 });
@@ -162,8 +162,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c929d5b0a90daa03fe31",
-    /*image:"",  */
-  price: "15",
+    image:"",  
+  price: 15,
   description:
     " Un juego de disparos en primera persona ambientado en la Segunda Guerra Mundial. Ofrece varias campañas que siguen a soldados de diferentes naciones y es conocido por su autenticidad histórica y acción trepidante.",
 });
@@ -176,8 +176,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c916d5b0a90daa03fe2f",
-    /*image:"",  */
-  price: "50",
+    image:"",  
+  price:50,
   description:
     "Un juego de rol de acción desarrollado por FromSoftware y George R.R. Martin. Promete un vasto mundo abierto lleno de peligros, combates desafiantes y una rica mitología, manteniendo la dificultad y diseño característicos de los juegos de FromSoftware.",
 });
@@ -190,8 +190,8 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c8f0d5b0a90daa03fe2d",
-   /* image:"",  */
-  price: "20",
+    image:"",  
+  price: 20,
   description:
     "Un juego de acción y aventura en mundo abierto que sigue las historias entrelazadas de tres criminales en la ciudad ficticia de Los Santos. Es conocido por su vasta y detallada representación de la vida urbana, así como su jugabilidad diversa y narrativa humorística.",
 });
@@ -204,10 +204,290 @@ await VideoGame.create({
     gender: "6647c2afa55c56098a0045b5",
     theme: "66484bf01593a60f58e0c0a3",
     developer:"6647c8f0d5b0a90daa03fe2d",
-    /*image:"",  */
-  price: "35",
+    image:"",  
+  price: 35,
   description:
     "Un juego de horror y supervivencia que sigue a Leon S. Kennedy en una misión para rescatar a la hija del presidente de Estados Unidos de un culto siniestro en una remota aldea europea. Es famoso por reinventar la serie con su enfoque en la acción y su innovador sistema de cámara.",
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290da",
+  name: "The Legend of Zelda: Breath of the Wild",
+  image: "",
+  price: 60,
+  description: "Un juego de aventura en un mundo abierto donde el jugador explora Hyrule y lucha contra el malvado Ganon."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290db",
+  name: "Minecraft",
+  image: "",
+  price: 26.95,
+  description: "Un juego sandbox que permite a los jugadores construir y explorar mundos generados procedimentalmente."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290dc",
+  name: "Fortnite",
+  image: "",
+  price: 0,
+  description: "Un juego de batalla real donde los jugadores compiten para ser el último en pie en una isla llena de peligros."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290dd",
+  name: "Super Mario Odyssey",
+  image: "",
+  price: 60,
+  description: "Una aventura de plataformas en 3D donde Mario viaja por varios mundos para rescatar a la princesa Peach."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290de",
+  name: "Red Dead Redemption 2",
+  image: "",
+  price: 60,
+  description: "Un juego de acción y aventura ambientado en el Lejano Oeste donde el jugador sigue la historia de Arthur Morgan."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290df",
+  name: "The Witcher 3: Wild Hunt",
+  image: "",
+  price: 40,
+  description: "Un RPG de mundo abierto donde el jugador controla a Geralt de Rivia en su búsqueda para encontrar a su hija adoptiva."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e0",
+  name: "Overwatch",
+  image: "",
+  price: 40,
+  description: "Un juego de disparos en equipo donde los jugadores eligen héroes con habilidades únicas para competir en diversos modos de juego."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e1",
+  name: "Call of Duty: Modern Warfare",
+  image: "",
+  price: 60,
+  description: "Un juego de disparos en primera persona que ofrece una intensa experiencia de combate moderno."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e2",
+  name: "Grand Theft Auto V",
+  image: "",
+  price: 30,
+  description: "Un juego de acción y aventura en un mundo abierto donde el jugador puede explorar la ciudad de Los Santos y participar en diversas actividades."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e3",
+  name: "Animal Crossing: New Horizons",
+  image: "",
+  price: 60,
+  description: "Un juego de simulación social donde los jugadores desarrollan y personalizan su propia isla."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e4",
+  name: "Doom Eternal",
+  image: "",
+  price: 60,
+  description: "Un juego de disparos en primera persona lleno de acción donde el jugador combate hordas de demonios."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e5",
+  name: "Cyberpunk 2077",
+  image: "",
+  price: 60,
+  description: "Un RPG de mundo abierto ambientado en un futuro distópico donde el jugador explora la ciudad de Night City."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e6",
+  name: "Halo Infinite",
+  image: "",
+  price: 60,
+  description: "Un juego de disparos en primera persona que sigue la historia del Master Chief en su lucha contra el Covenant."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e7",
+  name: "Assassin's Creed Valhalla",
+  image: "",
+  price: 60,
+  description: "Un juego de acción y aventura donde el jugador controla a un vikingo explorando la Inglaterra del siglo IX."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e8",
+  name: "Resident Evil Village",
+  image: "",
+  price: 60,
+  description: "Un juego de terror y supervivencia que sigue la historia de Ethan Winters en un misterioso pueblo europeo."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290e9",
+    name: "Hades",
+    image: "",
+    price: 25,
+    description: "Un juego de acción roguelike donde el jugador controla a Zagreus en su intento de escapar del Inframundo."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290ea",
+  name: "Among Us",
+  image: "",
+  price: 5,
+  description: "Un juego multijugador de deducción social donde los jugadores deben identificar al impostor en la nave espacial."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290eb",
+  name: "Ghost of Tsushima",
+  image: "",
+  price: 60,
+  description: "Un juego de acción y aventura ambientado en el Japón feudal donde el jugador controla a un samurái en su lucha contra los invasores mongoles."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290ec",
+  name: "God of War",
+  image: "",
+  price: 60,
+  description: "Un juego de acción y aventura que sigue la historia de Kratos y su hijo Atreus en la mitología nórdica."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290ed",
+  name: "Splatoon 2",
+  image: "",
+  price: 60,
+  description: "Un juego de disparos en tercera persona donde los jugadores compiten para cubrir el mayor área posible con tinta."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290ee",
+  name: "Persona 5",
+  image: "",
+  price: 60,
+  description: "Un juego de rol japonés que sigue la vida de un grupo de estudiantes de secundaria con habilidades sobrenaturales."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290ef",
+  name: "Final Fantasy VII Remake",
+  image: "",
+  price: 60,
+  description: "Una versión modernizada del clásico juego de rol japonés con gráficos mejorados y un sistema de combate actualizado."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f0",
+  name: "Sekiro: Shadows Die Twice",
+  image: "",
+  price: 60,
+  description: "Un juego de acción y aventura que sigue a un shinobi en su misión para rescatar a su señor y vengarse de sus enemigos."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f1",
+  name: "Mario Kart 8 Deluxe",
+  image: "",
+  price: 60,
+  description: "Un juego de carreras de karts con personajes de la serie Mario y varios modos de juego divertidos."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f2",
+  name: "Horizon Zero Dawn",
+  image: "",
+  price: 50,
+  description: "Un juego de acción y aventura en un mundo post-apocalíptico donde los jugadores cazan máquinas peligrosas."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f3",
+  name: "Bloodborne",
+  image: "",
+  price: 20,
+  description: "Un juego de rol y acción en un mundo gótico lleno de monstruos y secretos oscuros."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f4",
+  name: "The Elder Scrolls V: Skyrim",
+  image: "",
+  price: 40,
+  description: "Un RPG de mundo abierto donde los jugadores pueden explorar y vivir aventuras en la tierra de Skyrim."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f5",
+  name: "Dark Souls III",
+  image: "",
+  price: 60,
+  description: "Un desafiante juego de rol de acción con combates intensos y un mundo interconectado."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f6",
+  name: "Celeste",
+  image: "",
+  price: 20,
+  description: "Un juego de plataformas desafiante donde el jugador debe ayudar a Madeline a escalar una montaña mientras enfrenta sus propios demonios."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f7",
+  name: "Control",
+  image: "",
+  price: 30,
+  description: "Un juego de acción y aventura donde el jugador investiga fenómenos sobrenaturales en una misteriosa agencia gubernamental."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f8",
+  name: "Nier: Automata",
+  image: "",
+  price: 40,
+  description: "Un juego de rol y acción que explora temas filosóficos a través de la historia de androides en un mundo post-apocalíptico."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290f9",
+  name: "Monster Hunter: World",
+  image: "",
+  price: 30,
+  description: "Un juego de rol de acción donde los jugadores cazan monstruos gigantes en un mundo abierto."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290fa",
+  name: "Star Wars Jedi: Fallen Order",
+  image: "",
+  price: 60,
+  description: "Un juego de acción y aventura donde el jugador controla a un Jedi en su lucha contra el Imperio."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290fb",
+  name: "Cuphead",
+  image: "",
+  price: 20,
+  description: "Un juego de plataformas y disparos con un estilo artístico inspirado en los dibujos animados de los años 30."
+});
+
+await VideoGame.create({
+  _id: "664695bacf29fe32661290fc",
+  name: "Apex Legends",
+  image: "",
+  price: 0,
+  description: "Un juego de batalla real donde los jugadores compiten en equipos para ser los últimos en pie."
 });
 
 console.log("Juegos creados exitosamente");
