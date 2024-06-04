15fbbf20d158340305d92062a8dc48adc4ba44fb
DevelopmentSystem <gtovar@dsystem.co>
2024-06-04 15:34:24 -0500

    create middlewares, validates for pegi,theme and paymentMethod

--- routes/paymentMethodRoutes.js
+++ routes/paymentMethodRoutes.js
@@ -1,9 +1,11 @@
 import paymentMethodControllers from "../controllers/paymentMethodControllers.js";
 import express from "express";
+import paymentMethodValidations from "../middlewares/validatePaymentMethod.js";
+import errorsIsEmpty from "../middlewares/errorsIsEmpty.js"
 
 const router = express.Router();
 
-router.post("/api/paymentMethod",paymentMethodControllers.create);
+router.post("/api/paymentMethod",paymentMethodValidations,errorsIsEmpty,paymentMethodControllers.create);
 router.get("/api/paymentMethod/:id",paymentMethodControllers.find);
 router.patch("/api/paymentMethod/:id",paymentMethodControllers.update);
 router.get("/api/paymentMethods",paymentMethodControllers.list);
--- routes/pegiRoutes.js
+++ routes/pegiRoutes.js
@@ -1,11 +1,14 @@
 import pegiControllers from "../controllers/pegiControllers.js";
 import express from "express";
+import pegiValidations from "../middlewares/validatePegi.js";
+import errorsIsEmpty from "../middlewares/errorsIsEmpty.js"
 
 const router = express.Router();
 
-router.post("/api/pegi",pegiControllers.create);
+router.post("/api/pegi",pegiValidations,errorsIsEmpty,pegiControllers.create);
 router.get("/api/pegi/:id",pegiControllers.find);
 router.patch("/api/pegi/:id",pegiControllers.update);
 router.get("/api/pegis",pegiControllers.list);
 router.delete("/api/pegi/:id",pegiControllers.destroy);
 export default router;
+
