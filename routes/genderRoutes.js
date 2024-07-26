5dd2aaca0e85465174857c801611ae83bb86bf9a
cristiannull <coralcristian5@gmail.com>
2024-07-25 20:18:14 -0500

    cambiar text

--- routes/genderRoutes.js
+++ routes/genderRoutes.js
@@ -1,12 +1,17 @@
 import genderControllers from "../controllers/genderControllers.js";
 import express from "express";
-import genderValidations from "../middlewares/validategender.js";
-import errorsIsEmpty from "../middlewares/errorsIsEmpty.js"
+import genderValidations from "../middlewares/validateGender.js";
+import errorsIsEmpty from "../middlewares/errorsIsEmpty.js";
 const router = express.Router();
 
-router.post("/api/gender",genderValidations, errorsIsEmpty, genderControllers.create);
+router.post(
+  "/api/gender",
+  genderValidations,
+  errorsIsEmpty,
+  genderControllers.create
+);
 router.get("/api/gender/:id", genderControllers.find);
 router.patch("/api/gender/:id", genderControllers.update);
 router.get("/api/genders", genderControllers.list);
-router.delete("/api/gender/:id",genderControllers.destroy);
+router.delete("/api/gender/:id", genderControllers.destroy);
 export default router;
