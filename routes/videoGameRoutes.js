67ecae68dd0660d500395de42d3c29209528a821
cristiannull <coralcristian5@gmail.com>
2024-07-27 07:17:17 -0500

    delete code

--- routes/videoGameRoutes.js
+++ routes/videoGameRoutes.js
@@ -1,16 +1,10 @@
 import videogameControllers from "../controllers/videogameControllers.js";
 import express from "express";
-import fs from "fs";
-import path from "path";
+
 import videogameValidations from "../middlewares/validateVideogame.js";
 import errorsIsEmpty from "../middlewares/errorsIsEmpty.js";
 
 const router = express.Router();
-
-const uploadDir = path.join(import.meta.dirname, "../public/uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 router.post(
   "/api/videogame",
